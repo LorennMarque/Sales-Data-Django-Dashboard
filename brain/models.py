2a07d555c58194bc8fbcabb0749baa2cf9acec57
lorenzoMarquesini <lorenzomarquesini@gmail.com>
2024-05-23 20:22:32 -0300

    cargar datos
    Co-authored-by: juanchierasco <juanchierasco@users.noreply.github.com>

--- brain/models.py
+++ brain/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+import pandas as pd
 
 # Create your models here.
+data = pd.read_csv("data/supermarket_sales.csv")
+# print(data)
+
+# Create your models here.
+# class Question (models.Model):
+#     question_text = models.CharField(max_length=200)
+#     date = models.DateTimeField()
+
+#     def __str__(self):
+#         return self.question_text
+
+# class Answer (models.Model):
+#     answer_text = models.CharField(max_length=200)
+#     date = models.DateTimeField()
+#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+#     likes = models.IntegerField(default=0)
+
+#     def __str__(self):
+#         return self.answer_text
+    
+#     def is_popular (self):
+#         return self.likes >= 3
+
+class Sale(models.Model):
+    # Invoice ID,
+    invoice_id = models.CharField(max_length=255)
+    # Branch,
+    branch = models.CharField(max_length=1)
+    # City,
+    city = models.CharField(max_length=20)
+    # Customer type,
+    costumer_type = models.CharField(max_length=10) # Pero el máximo sería 6, Member ó Normal
+    # Gender,
+    gender = models.CharField(max_length=10) # Pero el máximo sería 6, Fermale ó Male
+    # Product line,
+    product_line = models.CharField(max_length=255)
+    # Unit price,
+    unit_price = models.FloatField()
+    # Quantity,
+    quantity = models.FloatField()
+    # Tax 5%,
+    tax = models.CharField(max_length=10) # Pero el máximo sería 6, Fermale ó Male
+    # Total,
+    total = models.FloatField()
+    # Date,
+    date = models.DateField()
+    # Time,
+    time = models.TimeField()
+    # Payment,
+    payment = models.CharField(max_length=10)
+    # cogs,
+    cogs = models.FloatField()
+    # gross margin percentage,
+    gross_margin_percentage = models.FloatField()
+    # gross income,
+    gross_income = models.FloatField()
+    # Rating
+    rating = models.FloatField()
--- brain/views.py
+++ brain/views.py
@@ -1,3 +1,63 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from brain.models import Sale
+import csv
+
+def visualize_data(request):
+    # sales = Sales.objects.order_by('-date') # menor a mayor default. Incluir el - para invertir
+    
+    with open ("data/supermarket_sales.csv" , "r") as f:
+        data = csv.reader(f)
+        sales = [] # Lista de objetos
+
+        for row in data:
+            # 1. Revisar si la primera fila es un encabezado      
+            is_header = row[0] == "Invoice ID"
+            
+            if not is_header:
+                created = Sale(
+                        invoice_id = row[0],
+                        branch = row[1],
+                        city = row[2],
+                        costumer_type = row[3], 
+                        gender = row[4], 
+                        product_line = row[5],
+                        unit_price = row[6],
+                        quantity = row[7],
+                        tax = row[8], 
+                        total = row[9],
+                        date = row[10],
+                        time = row[11],
+                        payment = row[12],
+                        cogs = row[13],
+                        gross_margin_percentage = row[14],
+                        gross_income = row[15],
+                        rating = row[16] )
+                # Agregamos la venta creada a la lista                # Agregamos la venta creada a la lista
+                sales.append(created)
+
+    context = {
+    "sales" : sales
+    }
+
+    return render(request, 'visualize_data.html', context)
+
+# from django.shortcuts import render
+# from django.http import HttpResponse
+# from django.template import loader
+
+# from pollapp.models import Question
+
+# def index(request):
+#     questions = Question.objects.order_by('-date') # menor a mayor default. Incluir el - para invertir
+
+#     context = {
+#         "questions":questions
+#     }
+
+#     return render(request, "index.html", context)
+
+# def detail(request, question_id):
+#     return HttpResponse(f"This is the question number {question_id}")
